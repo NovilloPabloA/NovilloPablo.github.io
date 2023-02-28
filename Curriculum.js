b828b365e105ac3d684d77bbec213f26ce39186e
NovilloPabloA <126607827+NovilloPabloA@users.noreply.github.com>
2023-02-28 15:53:43 -0300

    Add files via upload

---
+++ Curriculum.js
@@ -0,0 +1,26 @@
+document.getElementById('bt1').onclick = function () {    
+    document.getElementById("verdatos").style.display = 'block';
+    document.getElementById("verexperiencia").style.display = 'none';
+    document.getElementById("verformacion").style.display = 'none';
+    document.getElementById("vercontacto").style.display = 'none';
+}
+document.getElementById('bt2').onclick = function () {    
+    document.getElementById("verdatos").style.display = 'none';
+    document.getElementById("verexperiencia").style.display = 'block';
+    document.getElementById("verformacion").style.display = 'none';
+    document.getElementById("vercontacto").style.display = 'none';
+}
+document.getElementById('bt3').onclick = function () {    
+    document.getElementById("verdatos").style.display = 'none';
+    document.getElementById("verexperiencia").style.display = 'none';
+    document.getElementById("verformacion").style.display = 'block';
+    document.getElementById("vercontacto").style.display = 'none';
+}
+document.getElementById('bt4').onclick = function () {    
+    document.getElementById("verdatos").style.display = 'none';
+    document.getElementById("verexperiencia").style.display = 'none';
+    document.getElementById("verformacion").style.display = 'none';
+    document.getElementById("vercontacto").style.display = 'block';
+}
+
+    
